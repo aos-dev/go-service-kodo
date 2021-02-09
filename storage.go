d33a94929d316bd2586c9d6a39e51b22ea93a21c
Prnyself <281239768@qq.com>
2021-02-09 16:27:38 +0800

    *: Migrate to go-storage v3

--- storage.go
+++ storage.go
@@ -2,15 +2,16 @@ package kodo
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"net/http"
+	"strconv"
 	"time"
 
 	qs "github.com/qiniu/api.v7/v7/storage"
 
-	"github.com/aos-dev/go-storage/v2/pkg/iowrap"
-	"github.com/aos-dev/go-storage/v2/types"
-	"github.com/aos-dev/go-storage/v2/types/info"
+	"github.com/aos-dev/go-storage/v3/pkg/iowrap"
+	. "github.com/aos-dev/go-storage/v3/types"
 )
 
 func (s *Storage) delete(ctx context.Context, path string, opt *pairStorageDelete) (err error) {
@@ -22,100 +23,130 @@ func (s *Storage) delete(ctx context.Context, path string, opt *pairStorageDelet
 	}
 	return nil
 }
-func (s *Storage) listDir(ctx context.Context, dir string, opt *pairStorageListDir) (err error) {
-	marker := ""
-	delimiter := "/"
-	rp := s.getAbsPath(dir)
 
-	for {
-		entries, commonPrefix, nextMarker, _, err := s.bucket.ListFiles(s.name, rp, delimiter, marker, 1000)
-		if err != nil {
-			return err
-		}
-
-		if opt.HasDirFunc {
-			for _, v := range commonPrefix {
-				o := &types.Object{
-					ID:         v,
-					Name:       s.getRelPath(v),
-					Type:       types.ObjectTypeDir,
-					ObjectMeta: info.NewObjectMeta(),
-				}
-
-				opt.DirFunc(o)
-			}
-		}
-
-		if opt.HasFileFunc {
-			for _, v := range entries {
-				o, err := s.formatFileObject(v)
-				if err != nil {
-					return err
-				}
-
-				opt.FileFunc(o)
-			}
-		}
-
-		marker = nextMarker
-		if marker == "" {
-			return nil
-		}
+func (s *Storage) list(ctx context.Context, path string, opt *pairStorageList) (oi *ObjectIterator, err error) {
+	input := &objectPageStatus{
+		maxKeys: 1000,
+		prefix:  s.getAbsPath(path),
 	}
-}
-func (s *Storage) listPrefix(ctx context.Context, prefix string, opt *pairStorageListPrefix) (err error) {
-	marker := ""
-	rp := s.getAbsPath(prefix)
 
-	for {
-		entries, _, nextMarker, _, err := s.bucket.ListFiles(s.name, rp, "", marker, 1000)
-		if err != nil {
-			return err
-		}
+	var nextFn NextObjectFunc
 
-		for _, v := range entries {
-			o, err := s.formatFileObject(v)
-			if err != nil {
-				return err
-			}
-
-			opt.ObjectFunc(o)
-		}
-
-		marker = nextMarker
-		if marker == "" {
-			return nil
-		}
+	switch {
+	case opt.ListMode.IsDir():
+		input.delimiter = "/"
+		nextFn = s.nextObjectPageByDir
+	case opt.ListMode.IsPrefix():
+		nextFn = s.nextObjectPageByPrefix
+	default:
+		return nil, fmt.Errorf("invalid list mode")
 	}
+
+	return NewObjectIterator(ctx, nextFn, input), nil
 }
-func (s *Storage) metadata(ctx context.Context, opt *pairStorageMetadata) (meta info.StorageMeta, err error) {
-	meta = info.NewStorageMeta()
+
+func (s *Storage) metadata(ctx context.Context, opt *pairStorageMetadata) (meta *StorageMeta, err error) {
+	meta = NewStorageMeta()
 	meta.Name = s.name
 	meta.WorkDir = s.workDir
 	return meta, nil
 }
-func (s *Storage) read(ctx context.Context, path string, opt *pairStorageRead) (rc io.ReadCloser, err error) {
+
+func (s *Storage) nextObjectPageByDir(ctx context.Context, page *ObjectPage) error {
+	input := page.Status.(*objectPageStatus)
+
+	entries, commonPrefix, nextMarker, _, err := s.bucket.ListFiles(
+		s.name,
+		input.prefix,
+		input.delimiter,
+		input.marker,
+		input.maxKeys,
+	)
+	if err != nil {
+		return err
+	}
+
+	for _, v := range commonPrefix {
+		o := s.newObject(true)
+		o.ID = v
+		o.Path = s.getRelPath(v)
+		o.Mode |= ModeDir
+
+		page.Data = append(page.Data, o)
+	}
+
+	for _, v := range entries {
+		o, err := s.formatFileObject(v)
+		if err != nil {
+			return err
+		}
+
+		page.Data = append(page.Data, o)
+	}
+
+	if nextMarker == "" {
+		return IterateDone
+	}
+
+	input.marker = nextMarker
+	return nil
+}
+
+func (s *Storage) nextObjectPageByPrefix(ctx context.Context, page *ObjectPage) error {
+	input := page.Status.(*objectPageStatus)
+
+	entries, _, nextMarker, _, err := s.bucket.ListFiles(
+		s.name,
+		input.prefix,
+		input.delimiter,
+		input.marker,
+		input.maxKeys,
+	)
+	if err != nil {
+		return err
+	}
+
+	for _, v := range entries {
+		o, err := s.formatFileObject(v)
+		if err != nil {
+			return err
+		}
+
+		page.Data = append(page.Data, o)
+	}
+
+	if nextMarker == "" {
+		return IterateDone
+	}
+
+	input.marker = nextMarker
+	return nil
+}
+
+func (s *Storage) read(ctx context.Context, path string, w io.Writer, opt *pairStorageRead) (n int64, err error) {
 	rp := s.getAbsPath(path)
 
 	deadline := time.Now().Add(time.Hour).Unix()
 	url := qs.MakePrivateURL(s.bucket.Mac, s.domain, rp, deadline)
 	resp, err := s.bucket.Client.Get(url)
 	if err != nil {
-		return nil, err
+		return 0, err
 	}
 	if resp.StatusCode != http.StatusOK {
 		err = qs.ResponseError(resp)
-		return nil, err
+		return 0, err
 	}
 
-	rc = resp.Body
+	rc := resp.Body
 
-	if opt.HasReadCallbackFunc {
-		rc = iowrap.CallbackReadCloser(rc, opt.ReadCallbackFunc)
+	if opt.HasIoCallback {
+		rc = iowrap.CallbackReadCloser(resp.Body, opt.IoCallback)
 	}
-	return
+
+	return io.Copy(w, rc)
 }
-func (s *Storage) stat(ctx context.Context, path string, opt *pairStorageStat) (o *types.Object, err error) {
+
+func (s *Storage) stat(ctx context.Context, path string, opt *pairStorageStat) (o *Object, err error) {
 	rp := s.getAbsPath(path)
 
 	fi, err := s.bucket.Stat(s.name, rp)
@@ -123,29 +154,31 @@ func (s *Storage) stat(ctx context.Context, path string, opt *pairStorageStat) (
 		return nil, err
 	}
 
-	o = &types.Object{
-		ID:         rp,
-		Name:       path,
-		Type:       types.ObjectTypeFile,
-		Size:       fi.Fsize,
-		UpdatedAt:  convertUnixTimestampToTime(fi.PutTime),
-		ObjectMeta: info.NewObjectMeta(),
-	}
+	o = s.newObject(true)
+	o.ID = rp
+	o.Path = path
+	o.Mode |= ModeRead
+
+	o.SetLastModified(convertUnixTimestampToTime(fi.PutTime))
+	o.SetContentLength(fi.Fsize)
 
 	if fi.Hash != "" {
-		o.SetETag(fi.Hash)
+		o.SetEtag(fi.Hash)
 	}
 	if fi.MimeType != "" {
 		o.SetContentType(fi.MimeType)
 	}
 
-	setStorageClass(o.ObjectMeta, fi.Type)
+	sm := make(map[string]string)
+	sm[MetadataStorageClass] = strconv.Itoa(fi.Type)
+	o.SetServiceMetadata(sm)
 
 	return o, nil
 }
-func (s *Storage) write(ctx context.Context, path string, r io.Reader, opt *pairStorageWrite) (err error) {
-	if opt.HasReadCallbackFunc {
-		r = iowrap.CallbackReader(r, opt.ReadCallbackFunc)
+
+func (s *Storage) write(ctx context.Context, path string, r io.Reader, size int64, opt *pairStorageWrite) (n int64, err error) {
+	if opt.HasIoCallback {
+		r = iowrap.CallbackReader(r, opt.IoCallback)
 	}
 
 	rp := s.getAbsPath(path)
@@ -153,9 +186,9 @@ func (s *Storage) write(ctx context.Context, path string, r io.Reader, opt *pair
 	uploader := qs.NewFormUploader(s.bucket.Cfg)
 	ret := qs.PutRet{}
 	err = uploader.Put(ctx,
-		&ret, s.putPolicy.UploadToken(s.bucket.Mac), rp, r, opt.Size, nil)
+		&ret, s.putPolicy.UploadToken(s.bucket.Mac), rp, r, size, nil)
 	if err != nil {
-		return err
+		return
 	}
-	return nil
+	return size, nil
 }
